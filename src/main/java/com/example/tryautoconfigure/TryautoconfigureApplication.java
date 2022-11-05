4bbebd7d610ff00ae5a4f2ca13051468f27992fe
minami.yoshihiko <nannany@hey.jp>
2022-11-05 16:14:51 +0900

    initial

---
+++ src/main/java/com/example/tryautoconfigure/TryautoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryautoconfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryautoconfigureApplication.class, args);
+	}
+
+}
